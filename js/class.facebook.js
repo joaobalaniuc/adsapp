37429dcfe0f3cf816643e5f84be803b83f7c9d6b
João Williams Balaniúc <jwillbala@hotmail.com>
2016-12-02 09:59:03 -0200

    master

--- js/class.facebook.js
+++ js/class.facebook.js
@@ -3,89 +3,162 @@
 //==============================================
 
 var fb = {
-    post: function () {
-        facebookConnectPlugin.showDialog(
-                {
-                    method: "feed",
-                    picture: 'https://www.google.co.jp/logos/doodles/2014/doodle-4-google-2014-japan-winner-5109465267306496.2-hp.png',
-                    name: 'Test Post',
-                    message: 'First photo post',
-                    caption: 'Testing using phonegap plugin',
-                    description: 'Posting photo using phonegap facebook plugin'
-                },
-        function (response) {
-            alert(JSON.stringify(response));
-        },
-                function (response) {
-                    alert(JSON.stringify(response));
-                });
-    },
     login: function () {
-        facebookConnectPlugin.login(["email", "public_profile"], function (result) {
-            //alert(":) fb.login() = " + JSON.stringify(result));
-            //---
-            // nesta altura guardar id em session...
-            // profilefpupdate() guardará em local
-            //---
-            sessionStorage.fb_id = result.authResponse.userID;
+
+        //alert("login");
+
+        facebookConnectPlugin.login(['email', 'public_profile', 'user_birthday'], function (result) {
+
+            //alert("fb.login() = " + JSON.stringify(result));
+            /*localStorage.fb_id = result.authResponse.userID;
+             localStorage.fb_status = 'connected';*/
+
             localStorage.fb_token = result.authResponse.accessToken;
-            localStorage.fb_status = 'connected';
-            // updates
-            profileFb(); // frontend
-            profileFbUpdate(sessionStorage.fb_id, "in"); // database
-            // get rest of data
-            this.getUserInfo();
+
+            //facebookConnectPlugin.api("/me?fields=id,birthday,gender,first_name,middle_name,age_range,last_name,name,picture.width(400),email", [],
+            facebookConnectPlugin.api("/me?fields=id,email,birthday,gender,first_name,middle_name,last_name,picture.width(400)", [],
+                    function (result) {
+
+                        //alert("/me = " + JSON.stringify(result));
+                        //alert(result.picture.data.url);
+
+                        if (typeof result.email !== "undefined") {
+                            var email = result.email;
+                        }
+                        else {
+                            var email = result.id;
+                        }
+
+                        preloader();
+                        // RUN AJAX
+                        $.ajax({
+                            url: localStorage.server + "/user_facebook.php",
+                            data: {
+                                user_fb: result.id,
+                                user_fb_token: localStorage.fb_token,
+                                user_fb_pic: result.picture.data.url,
+                                user_pass: localStorage.fb_token,
+                                user_email: email,
+                                user_genre: result.gender,
+                                user_first_name: result.first_name,
+                                user_middle_name: result.middle_name,
+                                user_last_name: result.last_name
+                            },
+                            type: 'GET',
+                            dataType: 'jsonp',
+                            jsonp: 'callback',
+                            timeout: localStorage.timeout
+                        })
+                                .always(function () {
+                                    preloader(false);
+                                })
+
+                                .fail(function () {
+
+                                })
+
+                                .done(function (res) {
+                                    if (res !== null) {
+
+                                        if (res.error) {
+                                            alert(res.error);
+                                            return;
+                                        }
+                                        if (res.id) {
+                                            localStorage.fb_id = result.id;
+                                            localStorage.user_id = res.id;
+                                            localStorage.user_email = email;
+                                            localStorage.user_pass = localStorage.fb_token;
+                                            window.location.href = "index.html";
+                                        }
+
+                                    } // res not null
+                                }); // after ajax
+
+
+                    },
+                    function (error) {
+                        alert("/me failed = " + error);
+                    });
+            //
         }, function (err) {
-            alert('an error occured while trying to login. please try again. Err:' + enc(err));
+            alert('an error occured while trying to login. please try again. Err:' + err);
         });
     },
-    getUserInfo: function () {
-        facebookConnectPlugin.api(localStorage.fb_id + "/?fields=id,email,first_name,last_name,gender,picture,birthday", ["public_profile", "user_birthday"],
-                function (result) {
-                    alert(":) fb.getUserInfo() = " + JSON.stringify(result));
-                    localStorage.fb_id = result.id;
-                    localStorage.fb_first_name = result.first_name;
-                    localStorage.fb_last_name = result.last_name;
-                    localStorage.fb_gender = result.gender;
-                    localStorage.fb_email = result.email;
-                    localStorage.fb_birthday = result.birthday;
-                    //alert(localStorage.fb_email);
-                },
-                function (error) {
-                    alert("Failed: " + error);
-                });
-    },
-    getLoginStatus: function () {
+    /*,
+     getUserInfo: function () {
+     //facebookConnectPlugin.api(localStorage.fb_id + "/?fields=id,email,first_name,last_name,gender,picture,birthday", ["public_profile", "user_birthday"],
+     facebookConnectPlugin.api("/me", ["public_profile"],
+     function (result) {
+     alert("fb.getUserInfo() = " + JSON.stringify(result));
+     localStorage.fb_id = result.id;
+     localStorage.fb_first_name = result.first_name;
+     localStorage.fb_last_name = result.last_name;
+     localStorage.fb_gender = result.gender;
+     localStorage.fb_email = result.email;
+     localStorage.fb_birthday = result.birthday;
+     //alert(localStorage.fb_email);
+     },
+     function (error) {
+     alert("Failed: " + error);
+     });
+     },
 
-        facebookConnectPlugin.getLoginStatus(function (response) {
-            alert(":) fb.getLoginStatus() = " + JSON.stringify(response));
-            localStorage.fb_status = response.status;
+     getLoginStatusX: function () {
 
-            if (response.status === 'connected') {
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-                localStorage.fb_id = result.authResponse.userID;
-                localStorage.fb_token = result.authResponse.accessToken;
-                //alert("AUTH OK");
-                //return "OK MESMO";
-            } else if (response.status === 'not_authorized') {
-                //alert("NOT AUTH");
-            } else {
-                //alert("NOG LOGGED");
-            }
-        });
-    },
+     facebookConnectPlugin.getLoginStatus(
+     function (response) {
+
+     alert("fb.getLoginStatusX() = " + JSON.stringify(response));
+     localStorage.fb_status = response.status;
+     if (response.status === 'connected') {
+     var uid = response.authResponse.userID;
+     var accessToken = response.authResponse.accessToken;
+     localStorage.fb_id = result.authResponse.userID;
+     localStorage.fb_token = result.authResponse.accessToken;
+     alert("AUTH OK");
+     //return "OK MESMO";
+     } else if (response.status === 'not_authorized') {
+     alert("NOT AUTH");
+     } else {
+     alert("NOG LOGGED");
+     }
+     },
+     function (error) {
+     alert("Failed: " + error);
+     });
+
+     },
+     getLoginStatus: function () {
+
+     facebookConnectPlugin.getLoginStatus(function (response) {
+
+     //alert("fb.getLoginStatus() = ");
+     localStorage.fb_status = response.status;
+
+     if (response.status === 'connected') {
+     var uid = response.authResponse.userID;
+     var accessToken = response.authResponse.accessToken;
+     localStorage.fb_id = result.authResponse.userID;
+     localStorage.fb_token = result.authResponse.accessToken;
+     alert("AUTH OK");
+     //return "OK MESMO";
+     } else if (response.status === 'not_authorized') {
+     alert("NOT AUTH");
+     } else {
+     alert("NOG LOGGED");
+     }
+     });
+     }*/
     logout: function () {
         facebookConnectPlugin.logout(
                 function () {
-                    localStorage.removeItem("fb_id");
-                    sessionStorage.removeItem("fb_id");
-                    profileFb();
-                    profileFbUpdate(sessionStorage.fb_id, "out");
-                    //alert(":) logout ok");
+                    localStorage.removeItem("user_id");
+                    window.location.href = "index.html";
                 },
                 function () {
-                    alert(":( logout error");
+                    //alert("logout error");
                 });
     }
+
 };
