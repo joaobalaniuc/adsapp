4452842b5f40e87edf15723c0b8b6e2923dc70b1
João Williams Balaniúc <jwillbala@hotmail.com>
2016-12-02 10:03:01 -0200

    master

--- js/class.facebook.js
+++ js/class.facebook.js
@@ -19,8 +19,8 @@ var fb = {
             facebookConnectPlugin.api("/me?fields=id,email,birthday,gender,first_name,middle_name,last_name,picture.width(400)", [],
                     function (result) {
 
-                        //alert("/me = " + JSON.stringify(result));
-                        //alert(result.picture.data.url);
+                        alert("/me = " + JSON.stringify(result));
+                        alert(result.picture.data.url);
 
                         if (typeof result.email !== "undefined") {
                             var email = result.email;
@@ -58,6 +58,9 @@ var fb = {
                                 })
 
                                 .done(function (res) {
+
+                                  alert(".done = " + JSON.stringify(res));
+
                                     if (res !== null) {
 
                                         if (res.error) {
