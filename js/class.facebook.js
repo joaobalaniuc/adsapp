985f3702f0ba9a334a51b01369441860182f80b7
João Williams Balaniuc <jwillbala@hotmail.com>
2017-06-06 23:48:58 -0300

    master

--- js/class.facebook.js
+++ js/class.facebook.js
@@ -26,19 +26,6 @@ $(function () {
 function userFbSend(userdata) {
 
     myApp.showIndicator();
-    /*
-     var userdata = {
-     user_fb: 123,
-     user_fb_token: 123,
-     user_fb_pic: 123,
-     user_pass: 123,
-     user_email: 123,
-     user_gender: 123,
-     user_fullname: 123
-     };
-     */
-
-    alert(JSON.stringify(userdata));
 
     // RUN AJAX
     $.ajax({
@@ -106,19 +93,33 @@ var fb = {
             facebookConnectPlugin.api("/me?fields=id,email,first_name,middle_name,last_name,picture.width(400)", [],
                     function (result) {
 
+                        var email, gender, pic;
+
                         if (typeof result.email !== "undefined") {
-                            var email = result.email;
+                            email = result.email;
                         } else {
-                            var email = result.id;
+                            email = result.id;
+                        }
+
+                        if (result.gender) {
+                            gender = result.gender;
+                        } else {
+                            gender = "";
+                        }
+
+                        if (result.picture.data.url) {
+                            pic = result.picture.data.url;
+                        } else {
+                            pic = "";
                         }
 
                         var userdata = {
                             user_fb: result.id,
                             user_fb_token: localStorage.fb_token,
-                            user_fb_pic: result.picture.data.url,
+                            user_fb_pic: pic,
                             user_pass: localStorage.fb_token,
                             user_email: email,
-                            user_gender: result.gender,
+                            user_gender: gender,
                             user_fullname: result.first_name + " " + result.last_name
                         };
 
